7a0d8a438ea1899bc86438805acf8112571b24ae
Rudresh <rudresh.gaonkar.pc@gmail.com>
2025-03-27 23:12:23 +0530

    finally it works

--- backend/server.js
+++ backend/server.js
@@ -4,10 +4,6 @@ const session = require('express-session');
 const bodyParser = require('body-parser');
 const path = require('path');
 
-
-const { poolPromise, sql } = require('./config/db');
-
-
 const userRoutes = require('./routes/userRoutes');
 const expenseRoutes = require('./routes/expenseRoutes');
 const categoryRoutes = require('./routes/categoryRoutes');
@@ -15,7 +11,7 @@ const reportRoutes = require('./routes/reportRoutes');
 const dashboardRoutes = require('./routes/dashboardRoutes');
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080 || 5000;
 
 
 app.use(bodyParser.json());
@@ -60,10 +56,6 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../frontend/index.html'));
 });
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
-
 app.use((req, res, next) => {
   if (req.path.startsWith('/api/') && !req.session.user) {
     return res.status(401).json({ message: "Unauthorized. Please log in again." });
@@ -71,57 +63,6 @@ app.use((req, res, next) => {
   next();
 });
 
-// Function to check and create required tables if they do not exist
-async function initializeDatabase() {
-  try {
-    const pool = await poolPromise;
-
-    // Create Users table if it does not exist
-    await pool.request().query(`
-      IF NOT EXISTS (SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = 'Users')
-      BEGIN
-        CREATE TABLE Users (
-          id INT IDENTITY(1,1) PRIMARY KEY,
-          name VARCHAR(100),
-          email VARCHAR(100) UNIQUE,
-          password VARCHAR(255)
-        )
-      END
-    `);
-
-    
-    // Create Expenses table if it does not exist
-    await pool.request().query(`
-      IF NOT EXISTS (SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = 'Expenses')
-      BEGIN
-        CREATE TABLE Expenses (
-          id INT IDENTITY(1,1) PRIMARY KEY,
-          userId INT,
-          title VARCHAR(255),
-          amount DECIMAL(18,2),
-          categoryId INT FOREIGN KEY REFERENCES Categories(id),
-          Date DATE,
-          Notes VARCHAR(255)
-        )
-      END
-    `);
-
-    // Create Categories table if it does not exist
-    await pool.request().query(`
-      IF NOT EXISTS (SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = 'Categories')
-      BEGIN
-        CREATE TABLE Categories (
-          id INT IDENTITY(1,1) PRIMARY KEY,
-          name VARCHAR(100),
-        )
-      END
-    `);
-
-    console.log("Database tables ensured.");
-  } catch (error) {
-    console.error("Error initializing database tables:", error);
-  }
-}
-
-
-// initializeDatabase();
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
